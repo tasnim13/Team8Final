ed8c99717acc29bedaaccf2befb8f977ef750fae
Raymond <93847054+rayros25@users.noreply.github.com>
2025-03-30 15:30:17 -0400

    In the process of amulets and player forms

---
+++ Team_8_Final/Assets/Scripts/Amulet.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class Amulet : MonoBehaviour{
+
+      public GameHandler gameHandler;
+      //public playerVFX playerPowerupVFX;
+      public bool isHealthPickUp = true;
+      public bool isSpeedBoostPickUp = false;
+
+      public int amuletID = 0;
+    //   1 is Wadjet,  Cobra
+    //   2 is Khnum,   Ram
+    //   3 is Horus,   Falcon
+    //   4 is Sekhmet, Lioness
+
+      public int healthBoost = 50;
+      public float speedBoost = 2f;
+      public float speedTime = 2f;
+
+      void Start(){
+            gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
+            //playerPowerupVFX = GameObject.FindWithTag("Player").GetComponent<playerVFX>();
+      }
+
+      public void OnTriggerEnter2D (Collider2D other){
+            if (other.gameObject.tag == "Player"){
+                  GetComponent<Collider2D>().enabled = false;
+                  GetComponent<AudioSource>().Play();
+                  StartCoroutine(DestroyThis());
+
+                  switch (amuletID) {
+                    case 0:
+                        Debug.Log("picked up generic amulet!");
+                        break;
+                    case 1:
+                        Debug.Log("Picked up Amulet of Wadjet!");
+                        break;
+                    case 2:
+                        Debug.Log("Picked up Amulet of Khnum!");
+                        break;
+                    case 3:
+                        Debug.Log("Picked up Amulet of Horus");
+                        break;
+                    case 4:
+                        Debug.Log("Picked up Amulet of Sekhmet!");
+                        break;
+                    default:
+                        Debug.Log("UH OH! Unknown Amulet type");
+                        break;
+                  }
+
+                //   if (isHealthPickUp == true) {
+                //         // gameHandler.playerGetHit(healthBoost * -1);
+                //         //playerPowerupVFX.powerup();
+                //   }
+
+                //   if (isSpeedBoostPickUp == true) {
+                //         // other.gameObject.GetComponent<PlayerMove>().speedBoost(speedBoost, speedTime);
+                //         //playerPowerupVFX.powerup();
+                //   }
+            }
+      }
+
+      IEnumerator DestroyThis(){
+        // TODO: why wait?
+            yield return new WaitForSeconds(0.1f);
+            Destroy(gameObject);
+      }
+
+}
