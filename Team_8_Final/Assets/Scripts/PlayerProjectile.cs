67aaa631f919f421a9272b40a8572a9ac4e8b50a
Raymond <93847054+rayros25@users.noreply.github.com>
2025-04-12 16:42:35 -0400

    i got the visuals for roar working but they stopped working

---
+++ Team_8_Final/Assets/Scripts/PlayerProjectile.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour{
+
+      public int damage = 1;
+    //   public GameObject hitEffectAnim;
+      public float SelfDestructTime = 0.3f;
+      public float SelfDestructVFX = 0.5f;
+      public SpriteRenderer projectileArt;
+      public GameObject splatterPrefab;
+
+      void Start(){
+           projectileArt = GetComponentInChildren<SpriteRenderer>();
+           StartCoroutine(SelfDestruct());
+      }
+
+      //if the bullet hits a collider, play the explosion animation, then destroy the effect and the bullet
+      void OnTriggerEnter2D(Collider2D other){
+            if (other.gameObject.layer == LayerMask.NameToLayer("Enemy")) {
+                Debug.Log("PlayerProjectile: enemy hit");
+                  //gameHandlerObj.playerGetHit(damage);
+                //   other.gameObject.GetComponent<EnemyMeleeDamage>().TakeDamage(damage);
+            }
+        //    if (other.gameObject.tag != "Player") {
+        // //    if (other.gameObject.tag != "Enemy" || other.gameObject.tag != "Player") {
+        //           gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        //         //   GameObject animEffect = Instantiate (hitEffectAnim, transform.position, Quaternion.identity);
+        //         //   projectileArt.SetActive(false);
+        //           //Destroy (animEffect, 0.5);
+        //         //   StartCoroutine(SelfDestructHit(animEffect));
+        //           StartCoroutine(SelfDestruct());
+            // }
+      }
+
+      IEnumerator SelfDestructHit(GameObject VFX){
+            //MakeSplat();
+            yield return new WaitForSeconds(SelfDestructVFX);
+            Destroy (VFX);
+            Destroy (gameObject);
+      }
+
+      IEnumerator SelfDestruct(){
+            yield return new WaitForSeconds(SelfDestructTime);
+            Destroy (gameObject);
+      }
+
+      /*
+      //Make a mark on the ground where the projectile hit
+      void MakeSplat(){
+            GameObject splat = Instantiate (splatterPrefab, transform.position, Quaternion.identity);
+            float zRotation = Random.Range(0f,179f);
+            splat.transform.eulerAngles = new Vector3(0, 0, zRotation);
+            float size = Random.Range(0.5f,0.9f);
+            splat.transform.localScale = new Vector3(splat.transform.localScale.x * size, splat.transform.localScale.y * size, 1);
+      }
+      */
+} 
--- Team_8_Final/Assets/Scripts/PlayerSpecialAttack.cs
+++ Team_8_Final/Assets/Scripts/PlayerSpecialAttack.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSpecialMove : MonoBehaviour
+public class PlayerSpecialAttack : MonoBehaviour
 {
 
     public GameObject projectilePrefab;
     public Transform firePoint;
     public float projectileSpeed = 10f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     // TODO: restrict to form
@@ -26,11 +20,20 @@ public class PlayerSpecialMove : MonoBehaviour
 
 
     void roarAttack() {
+        // number of projectiles
+        int num = 16;
         // TODO: ??
         // Vector2 fwd = (firePoint.position - this.transform.position).normalized; 
+        // Debug.Log("roar attack!!!");
         Vector2 fwd = Vector2.up;
 
-        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
+        // GameObject projectile;
+        
+        for (int i = 0; i < num; i++) {
+            GameObject projectile = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
             projectile.GetComponent<Rigidbody2D>().AddForce(fwd * projectileSpeed, ForceMode2D.Impulse); 
+            projectile.GetComponentInChildren<SpriteRenderer>().transform.Rotate(new Vector3(0, 0, i * (360f / num)));
+            fwd = Quaternion.AngleAxis(360f / num, Vector3.forward) * fwd;
+        }
     }
 }
