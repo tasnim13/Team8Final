ed52fc501e70d93b5e06c70258e3f4a4483fa6c7
tasnim13 <tasnim.hossain@tufts.edu>
2025-05-03 17:04:12 -0400

    key dropper works

--- Team_8_Final/Assets/Scripts/KeyDropper.cs
+++ Team_8_Final/Assets/Scripts/KeyDropper.cs
@@ -9,15 +9,33 @@ public class KeyDropper : MonoBehaviour
 
     void Update()
     {
-        if (!hasDropped && targetEnemy != null && targetEnemy.IsDead())
+        if (!hasDropped && targetEnemy != null)
         {
-            DropKey();
+            if (targetEnemy.IsDead())
+            {
+                Debug.Log($"[KeyDropper] {targetEnemy.name} is dead. Dropping key.");
+                DropKey();
+            }
+            else
+            {
+                Debug.Log($"[KeyDropper] {targetEnemy.name} is still alive. Key not yet dropped.");
+            }
         }
     }
 
     void DropKey()
     {
         hasDropped = true;
-        Instantiate(keyPrefab, transform.position, Quaternion.identity);
+
+        if (keyPrefab != null && targetEnemy != null)
+        {
+            Vector3 dropPosition = targetEnemy.transform.position;
+            Instantiate(keyPrefab, dropPosition, Quaternion.identity);
+            Debug.Log($"[KeyDropper] Key dropped at position {dropPosition}.");
+        }
+        else
+        {
+            Debug.LogWarning("[KeyDropper] KeyPrefab or targetEnemy was null! Cannot drop key.");
+        }
     }
 }
